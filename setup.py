178ee6ccf309d35b3a69835b05cf2630c4c766b0
MiaMiya <s183998@student.dtu.dk>
2023-01-06 11:12:09 +0100

    Add cookie defalut format

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of our project is to classify if a tweet was made by Trump or a Russian troll account.',
+    author='Group 8',
+    license='MIT',
+)
