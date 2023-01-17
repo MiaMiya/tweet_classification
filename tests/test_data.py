5b247f001fba840afbd3747bc7dbe86d9c5510d1
MiaMiya <s183998@student.dtu.dk>
2023-01-17 10:27:21 +0100

    update such data is optional

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,17 +4,17 @@ import os.path
 import pytest
 import numpy as np 
 
-# @pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed/train_processed.npy'), reason="Data files not found")
-# @pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed/test_processed.npy'), reason="Data files not found")
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed/train_processed.npy'), reason="Data files not found")
+@pytest.mark.skipif(not os.path.exists(f'{_PATH_DATA}/processed/test_processed.npy'), reason="Data files not found")
 
-dataset = Tweets(in_folder="data/raw",out_folder="data/processed")
 
 def test_data_len():
+    dataset = Tweets(in_folder="data/raw",out_folder="data/processed")
     assert len(dataset.train_tweet) == len(dataset.train_label), "Training dataset have uneven number of images and labels"
     assert len(dataset.test_tweet) == len(dataset.test_label), "Testing dataset have uneven number of images and labels"
 
 def test_data_type():
-
+    dataset = Tweets(in_folder="data/raw",out_folder="data/processed")
     assert (isinstance(item, int) for item in dataset.train_label), "Non integer values found in train labels"
     assert (isinstance(item, int) for item in dataset.test_label), "Non integer values found in test labels"
     assert (isinstance(item, str) for item in dataset.train_tweet), "Non string values foudn for the training tweets"
@@ -23,6 +23,7 @@ def test_data_type():
     assert set(dataset.test_label) == {1,0}, "Values other than 0 or 1 are found in test set labels"
 
 def test_data_na():
+    dataset = Tweets(in_folder="data/raw",out_folder="data/processed")
     assert dataset.train_tweet.isnull().sum() == 0    
 
 if __name__ == "__main__":
