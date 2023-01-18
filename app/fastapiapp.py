d1f473f2c767a1c2ac835d8f896d9a45a368199a
haraldsr <haraldsr@hotmail.com>
2023-01-18 15:27:13 +0100

    added upload txt to app

--- app/fastapiapp.py
+++ app/fastapiapp.py
@@ -3,6 +3,7 @@ from google.cloud import storage
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import io
+from fastapi import UploadFile, File
 
 BUCKET_NAME = "tweet_classification"
 MODEL_FILE = "my_trained_model.pt"
@@ -42,6 +43,39 @@ def read_item(tweet: str):
         input_ids=tokenized_tweet["input_ids"],
         attention_mask=tokenized_tweet["attention_mask"],
     )
+    log_probs = outputs.logits[0]  ## input CR
+    probs = log_probs.softmax(dim=-1).detach().cpu().flatten().numpy()
+    if probs[0] > 0.5:
+        pred = "Russian"
+        prob = str(round(probs[0], 2))
+    else:
+        pred = "Trump"
+        prob = str(round(probs[1], 2))
+
+    response = {"Prediction": pred, "With probability": prob, "Tweet": tweet}
+    return response
+
+@app.post("/upload/")
+async def cv_model(data: UploadFile = File(...)):
+    with open('tweet.txt', 'wb') as tweet:
+        content = await data.read()
+        tweet.write(content)
+        tweet.close()
+
+    tweet = open("tweet.txt", "r")
+
+    tokenized_tweet = tokenizer(
+        tweet,
+        max_length=512,
+        padding="max_length",
+        truncation=True,
+        return_tensors="pt",
+    )
+    outputs = model(
+        input_ids=tokenized_tweet["input_ids"],
+        attention_mask=tokenized_tweet["attention_mask"],
+    )
+
     log_probs = outputs.logits[0]  ## input CR
     probs = log_probs.softmax(dim=-1).detach().cpu().flatten().numpy()
     if probs[0] > 0.5:
